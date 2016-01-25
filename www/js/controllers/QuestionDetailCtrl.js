e16e7bc18c540efc6b75b216cf947d658775f3d4
guvenatbakan <guvenatbakan@gmail.com>
2016-01-25 23:54:10 +0200

    camera working now

--- www/js/controllers/QuestionDetailCtrl.js
+++ www/js/controllers/QuestionDetailCtrl.js
@@ -1,33 +1,67 @@
-ResminApp.controller('QuestionDetailCtrl', function QuestionDetailCtrl($scope, $route, $location, $routeParams, localStorageService, Api) {
+ResminApp.controller('QuestionDetailCtrl', function QuestionDetailCtrl($scope, $route, $location, $routeParams, $cordovaCamera, localStorageService, Api) {
     $scope.list = {};
     $scope.item = {};
 
     $scope.id = $routeParams.id;
+    $scope.uploadPreview = false;
+    $scope.base64Image = '';
+    $scope.imagePath = '';
 
-    $scope.loadQuestion = function(){
+    $scope.loadQuestion = function () {
         var data = {
             id: $scope.id,
         }
         Api.questionDetail(data,
-            function (successResponse){
+            function (successResponse) {
                 $scope.item = successResponse.data;
                 $scope.loadStoryList();
             },
-            function (errorResponse){
+            function (errorResponse) {
                 alert('hata');
             }
         );
     }
 
-    $scope.loadStoryList = function(){
+    $scope.cancelUpload = function () {
+        $scope.uploadPreview = false;
+        $scope.base64Image = "";
+    }
+
+    $scope.tookPhoto = function () {
+        var options = {
+            quality: 95,
+            //destinationType: Camera.DestinationType.DATA_URL,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: true,
+            correctOrientation: false
+        };
+
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            //console.log(imageData);
+            $scope.uploadPreview = true;
+            $scope.imagePath = imageData;
+        }, function (err) {
+            // error
+        });
+
+        $cordovaCamera.cleanup().then(function(){
+
+        });
+    }
+
+    $scope.loadStoryList = function () {
         var data = {
             id: $scope.id,
         }
         Api.questionStoryList(data,
-            function (successResponse){
+            function (successResponse) {
                 $scope.list = successResponse;
             },
-            function (errorResponse){
+            function (errorResponse) {
                 alert('hata');
             }
         );
